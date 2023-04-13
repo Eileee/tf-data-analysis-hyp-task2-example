208eb7ad5b0391c082dfd364cff765546b6ea80e
Eileee <130185869+Eileee@users.noreply.github.com>
2023-04-13 23:54:00 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as st
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 417796486 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    pv = st.ks_2samp(x,y, alternative ='two-sided')[1]
+    if (pv < 0.03):
+        return False
+    else:
+        return True
